373740806f2afb3bb876494c2c8e7af655ac6886
Matthew Le <matthewthaile@gmail.com>
2021-12-28 19:53:24 -0800

    header layout version 1.0

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+import mountainImage from '../../assets/mountains.jpg';
 
 const Header = props => {
     return <React.Fragment>
         <header className={classes.header}>
             <h1>RGI OUTDOORS</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
-            <img alt='rgi background logo'/>
+            <img src={mountainImage} alt='rgi background logo'/>
         </div>
 
     </React.Fragment>
